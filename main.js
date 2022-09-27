8bdce968062c39f3adea25c79bdf45c1e2d574e2
jjcqz <114376176+jjcqz@users.noreply.github.com>
2022-09-27 13:28:00 +1000

    Contact form added

--- main.js
+++ main.js
@@ -40,3 +40,17 @@ function closeModals() {
 function getAll(selector) {
     return Array.prototype.slice.call(document.querySelectorAll(selector), 0);
 }
+
+
+// contact form js below (note: below is copied from https://www.w3schools.com/howto/howto_js_snackbar.asp )
+function myFunction() {
+    // Get the snackbar DIV
+    var x = document.getElementById("snackbar");
+
+    // Add the "show" class to DIV
+    x.className = "show";
+  
+    // After 3 seconds, remove the show class from DIV
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+    return false;
+  }

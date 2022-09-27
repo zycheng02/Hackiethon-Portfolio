a18f2ed2a0c939b1c862974efbaa3a95e6c2ccf5
zycheng02 <zdcheng@student.unimelb.edu.au>
2022-09-27 21:55:33 +1000

    Revert "Merge branch 'main' of https://github.com/zycheng02/Hackiethon-Portfolio"
    This reverts commit f68d2c035382d4afe9c82091238fadc7c7af832c, reversing
    changes made to 03b89a34432b311aa9686d5532a491eabd598eb3.

--- main.js
+++ main.js
@@ -5,13 +5,6 @@ var $modals = getAll('.modal');
 var $modalTriggers = getAll('.modal-trigger');
 var $modalCloses = getAll('.modal-card-head .delete, .modal-card-foot .button');
 
-
-console.log($modals);
-
-console.log($modalTriggers);
-
-console.log($modalCloses);
-
 if ($modalTriggers.length > 0) {
     $modalTriggers.forEach(function ($el) {
         $el.addEventListener('click', function () {
@@ -47,17 +40,3 @@ function closeModals() {
 function getAll(selector) {
     return Array.prototype.slice.call(document.querySelectorAll(selector), 0);
 }
-
-
-// contact form js below (note: below is copied from https://www.w3schools.com/howto/howto_js_snackbar.asp )
-function myFunction() {
-    // Get the snackbar DIV
-    var x = document.getElementById("snackbar");
-
-    // Add the "show" class to DIV
-    x.className = "show";
-  
-    // After 3 seconds, remove the show class from DIV
-    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
-    return false;
-  }
